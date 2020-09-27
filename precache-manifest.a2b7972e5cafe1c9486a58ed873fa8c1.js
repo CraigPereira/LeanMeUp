1dd7cb9895926efc42c10eca6161da15df315701
Craig <craigpereira1337@gmail.com>
2020-09-27 23:53:29 +0530

    Updates

---
+++ precache-manifest.a2b7972e5cafe1c9486a58ed873fa8c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb57c716bfeb41dcabd3db808fd48726",
+    "url": "/LeanMeUp/index.html"
+  },
+  {
+    "revision": "4efb468445696584f2b8",
+    "url": "/LeanMeUp/static/css/main.4906a384.chunk.css"
+  },
+  {
+    "revision": "f62d898a49313362c439",
+    "url": "/LeanMeUp/static/js/2.1c44eb64.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/LeanMeUp/static/js/2.1c44eb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4efb468445696584f2b8",
+    "url": "/LeanMeUp/static/js/main.bedfd9b1.chunk.js"
+  },
+  {
+    "revision": "5b6d40178feeb5f6b0a1",
+    "url": "/LeanMeUp/static/js/runtime-main.4ddacbe1.js"
+  },
+  {
+    "revision": "1244b4e4fc812f7f471008e2554df973",
+    "url": "/LeanMeUp/static/media/LMU-Logo.1244b4e4.png"
+  },
+  {
+    "revision": "33039a69ef0468c9539abda43d9cf81c",
+    "url": "/LeanMeUp/static/media/crunch-1.33039a69.png"
+  },
+  {
+    "revision": "1c2b94d0de7e1acc4b53641f86ab5250",
+    "url": "/LeanMeUp/static/media/food-pyramid.1c2b94d0.png"
+  },
+  {
+    "revision": "5091057c1866a15a2d6df50f586d1b67",
+    "url": "/LeanMeUp/static/media/healthy-food.5091057c.png"
+  }
+]);
