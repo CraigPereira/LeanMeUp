71088b84622a1687f6fb19ba5c4fecbedd49b674
Craig Pereira <craigpereira1337@gmail.com>
2021-05-13 21:44:57 +0530

    Added deployed API baseUrl to frontend

--- Client/src/Axios/baseUrl.js
+++ Client/src/Axios/baseUrl.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:5000/" });
+const API = process.env.REACT_APP_API || "http://localhost:5000/";
+
+const instance = axios.create({ baseURL: API });
 
 instance.defaults.withCredentials = true;
 
