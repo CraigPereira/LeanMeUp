05fa363a6a7a3dd1dbe666dd36dca5e10f34f944
Craig Pereira <craigpereira1337@gmail.com>
2021-05-14 14:31:38 +0530

    Make it better

--- Server/Server.js
+++ Server/Server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 // );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://lean-me-up.netlify.app/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://lean-me-up.netlify.app"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
